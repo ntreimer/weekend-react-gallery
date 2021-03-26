1bee455d7594ec3e644c3a7f5b94d654042eaac3
Nate Treimer <natetreimer@gmail.com>
2021-03-26 18:13:28 -0500

    on click, now shows description instead

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import './GalleryItem.css'
+import './GalleryItem.css';
+import {useState} from 'react';
 
 function GalleryItem(props) {
+    const [showDescription, setShowDescription] = useState(false);
     console.log(props.image);
+    let displayDescription = () => {
+        if(showDescription) {
+            return props.image.description;
+        }
+        else {
+            return <img src={props.image.path} id={props.image.id} height="200px" width="200px"/>
+        }
+    }
+    let toggleDescription = () => {
+        setShowDescription(!showDescription);
+    }
     return (
-        <div className="picture"><img src={props.image.path} id={props.image.id} height="100px" width="100px"/></div>
+        <div className="picture" height="200px" width="200px">
+            <div onClick={toggleDescription}>{displayDescription()}</div>
+            <p>Likes: {props.image.likes}</p>
+            <button type="button">Like</button>
+        </div>
     );
 }
 
